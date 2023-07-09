aeff7a8749bdf22d630b213644c73fe55ec6f477
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-07-09 18:40:43 -0400

    added SECRET_KEY to the environmental file

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$!lk#+fh4qj!j!l2ovq(=6$*56uyiikq&arqap^k((+m#2b4x'
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
