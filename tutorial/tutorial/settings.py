0a66b7e9a3202d351db42c6535eb03f4e047de7c
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-07-14 13:15:32 -0400

    heroku again

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -58,7 +58,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets'
+    'corsheaders',
+    'snippets.apps.SnippetsConfig',
 ]
 
 REST_FRAMEWORK = {
